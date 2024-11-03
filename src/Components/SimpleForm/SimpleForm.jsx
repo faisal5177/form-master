4b33a662f30080d0ebd8916172e2c5840a604b9c
faisal <mdfaisal51777@gmail.com>
2024-11-04 01:15:56 +0600

    first commit

---
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,27 @@
+
+const SimpleForm = () => {
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        console.log(e.target.name.value); //akhane name and email er vetore ki lekha hoyache seta dekhabe .dekhanor neyom..
+        console.log(e.target.email.value); //e.target ta form take bujhai
+        console.log(e.target.phone.value); // properter name nebo and value debo
+        console.log('form submitted');
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name"/>
+                <br />
+                <input type="email" name="email"/>
+                <br />
+                <input type="text" name="phone"/>
+                <br />
+                <input type="submit" value="submit" />
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
