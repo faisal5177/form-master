526bdabd5091a4c4c40f106cbc6617421c9e1326
faisal <mdfaisal51777@gmail.com>
2024-11-05 02:57:54 +0600

    added an ReusableForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
 
 import './App.css'
-import StateFulForm from './Components/StateFulForm/StateFulForm'
+// import HookForm from './Components/HookForm/HookForm'
+import ReusableForm from './Components/ReusableForm/ReusableForm';
+// import Refform from './Components/Refform/Refform'
+// import StateFulForm from './Components/StateFulForm/StateFulForm'
 // import SimpleForm from './Components/SimpleForm/SimpleForm'
 
 function App() {
+
+  const handleSingUpSubmit = data =>{
+    console.log('sing up data', data);
+}
+  const  handleUpdateProfile = data =>{
+    console.log('update profile', data);
+  }
 
   return (
     <>
       
       <h1>Vite + React</h1>
       {/* <SimpleForm></SimpleForm> */}
-      <StateFulForm></StateFulForm>
+      {/* <StateFulForm></StateFulForm> */}
+      {/* <Refform></Refform> */}
+      {/* <HookForm></HookForm> */}
+      <ReusableForm 
+      formTitle={`Sing Up`} 
+      handleSubmit={handleSingUpSubmit}
+      >
+        <div>
+          <h2>Sing Up</h2>
+          <p>please sing up right now</p>
+        </div>
+      </ReusableForm>
+      <ReusableForm 
+      formTitle={`Profile Update`} 
+      handleSubmit={handleUpdateProfile} 
+      submitBtnText = 'Update'
+      ></ReusableForm>
     </>
   )
 }
 
-export default App
+export default App;
